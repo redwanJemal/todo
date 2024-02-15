5160b22aca49a641a8bc6b9a4c9d4236e2376f06
redwanJ <contact.redone.in@gmail.com>
2024-02-15 07:23:15 +0530

    show add component on todo list > 0

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
 	return (
 		<div className='flex justify-center'>
 			<header className='flex gap-2 bg-gray-600 rounded-md px-4 py-2 items-center'>
-				<img src={logo} className='w-5 h-5 text-white' alt='' />
+				<img src={logo} className='w-5 h-5' alt='' />
 				<span className='text-white text-lg font-bold'>My ToDo App</span>
 			</header>
 		</div>
--- client/src/hooks/useTodoApi.jsx
+++ client/src/hooks/useTodoApi.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import axios from 'axios'
 import { useState } from 'react'
+import { useTodos } from '../context/TodoContext'
 
 const api = axios.create({
 	baseURL: 'https://localhost:7193/api',
@@ -10,6 +11,7 @@ export const useTodoApi = () => {
 	const [todos, setTodos] = useState([])
 	const [pagingData, setPagingData] = useState([])
 	const [isLoading, setIsLoading] = useState(false)
+	const { setShowAddNew } = useTodos()
 
 	const fetchTodos = () => {
 		setIsLoading(true)
@@ -17,6 +19,7 @@ export const useTodoApi = () => {
 			.get('/todos')
 			.then((response) => {
 				setTodos(response.data?.data)
+				setShowAddNew(response.data?.data.length > 0)
 				setPagingData({
 					currentPage: response.data?.currentPage,
 					totalPages: response.data?.totalPages,
